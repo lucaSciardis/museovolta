21c9ab92aed400a1a68a279a0a6c4fb5278aa301
LUCA SCIARDIS <sciardis.luca@volta.ts.it>
2023-01-17 11:28:27 +0100

    Aggiunto l'HEADER della pagina

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Museo Volta</title>
+    <meta charset="utf-8q">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg ">
+        <div class="container-fluid">
+            <a href="index.php" class="navbar-brand">
+                <img src="thegroke.png" height="50" alt="Museo Informatico Volta">Museo Informatico Volta
+            </a>
+            <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarCollapse">
+                <div class="navbar-nav">
+                    <a href="index.php" class="nav-item nav-link">Home</a>
+                    <a href="#" class="nav-item nav-link">Contattaci</a>
+                    <a href="#" class="nav-item nav-link">Orari</a>
+                    <a href="#" class="nav-item nav-link">Su di noi</a>
+                </div>
+
+    </nav>
+  
+    </div>
+
+
+
+
+</body>
